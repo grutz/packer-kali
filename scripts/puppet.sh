08ae51a235b95ada09e0ab282d83247101face11
svigneux <svigneux@kronostechnologies.com>
2014-02-27 22:54:54 -0500

    Kali64 Packer files

---
+++ scripts/puppet.sh
@@ -0,0 +1,13 @@
+VERSION=3.4.3-1puppetlabs1 
+
+# Prepare puppetlabs repo
+wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+dpkg -i puppetlabs-release-wheezy.deb
+apt-get update
+rm -Rf puppetlabs-release-wheezy.deb
+
+# Install puppet/facter
+apt-get install -y puppet=$VERSION puppet-common=$VERSION
+sed -i -re "/\[main\]/ apluginsync = true" /etc/puppet/puppet.conf;
+
+
