67ffa6cc86c1fa1447e5a4dd605e5fb7fc880404
svigneux <svigneux@kronostechnologies.com>
2014-03-15 17:13:21 -0400

    remove white lines

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -9,5 +9,3 @@ rm -f puppetlabs-release-wheezy.deb
 # Install puppet/facter
 apt-get install -y puppet=$VERSION puppet-common=$VERSION
 sed -i -re "/\[main\]/ apluginsync = true" /etc/puppet/puppet.conf;
-
-
